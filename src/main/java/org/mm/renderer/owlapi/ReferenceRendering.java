8d6e42c7bd36f44fd604d4594d7561328f723a21
martinjoconnor <martin.oconnor@stanford.edu>
2015-07-13 13:02:57 -0700

    Some OWLAPI name alignment

--- src/main/java/org/mm/renderer/owlapi/ReferenceRendering.java
+++ src/main/java/org/mm/renderer/owlapi/ReferenceRendering.java
@@ -1,4 +1,41 @@
 package org.mm.renderer.owlapi;
 
+import org.semanticweb.owlapi.model.OWLEntity;
+import org.semanticweb.owlapi.model.OWLLiteral;
+
+import java.util.Optional;
+
 public class ReferenceRendering extends OWLAPIRendering
-{}
+{
+  private Optional<OWLLiteral> literal = Optional.empty();
+  private Optional<OWLEntity> entity = Optional.empty();
+
+  public void setOWLLiteral(OWLLiteral literal)
+  {
+    this.literal = Optional.of(literal);
+    this.entity = Optional.empty();
+  }
+
+  public void setOWLEntity(OWLEntity entity)
+  {
+    this.entity = Optional.of(entity);
+    this.literal = Optional.empty();
+  }
+
+  public boolean isOWLLiteral() { return this.literal.isPresent(); }
+
+  public boolean isOWLEntity() { return this.entity.isPresent(); }
+
+  public boolean isOWLClass() { return this.entity.isPresent() && this.entity.get().isOWLClass(); }
+
+  public boolean isOWLNamedIndividual() { return this.entity.isPresent() && this.entity.get().isOWLNamedIndividual(); }
+
+  public boolean isOWLObjectProperty() { return this.entity.isPresent() && this.entity.get().isOWLObjectProperty(); }
+
+  public boolean isOWLDataProperty() { return this.entity.isPresent() && this.entity.get().isOWLDataProperty(); }
+
+  public boolean isOWLAnnotationProperty() { return this.entity.isPresent() && this.entity.get().isOWLAnnotationProperty(); }
+
+  public boolean isOWLDatatype() { return this.entity.isPresent() && this.entity.get().isOWLDatatype(); }
+}
+
