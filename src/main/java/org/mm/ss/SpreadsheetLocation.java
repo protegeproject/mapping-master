e04ea8141349728ebc56d3e1a7f5b66d15a3fc11
martinjoconnor <martin.oconnor@stanford.edu>
2015-07-23 14:21:12 -0700

    Added base support class for integration tests

--- src/main/java/org/mm/ss/SpreadsheetLocation.java
+++ src/main/java/org/mm/ss/SpreadsheetLocation.java
@@ -1,73 +1,77 @@
-
 package org.mm.ss;
-
-import jxl.Sheet;
 
 public class SpreadsheetLocation
 {
-	// There is an equals() method defined on this class.
-	private final String sheetName;
-	private final int columnNumber, rowNumber;
+  // There is an equals() method defined on this class.
+  private final String sheetName;
+  private final int columnNumber, rowNumber;
 
-	public SpreadsheetLocation(String sheetName, int columnNumber, int rowNumber)
-	{
-		this.sheetName = sheetName;
-		this.columnNumber = columnNumber;
-		this.rowNumber = rowNumber;
-	}
+  /**
+   *
+   * @param sheetName The name of the sheet
+   * @param columnNumber 1-based column number
+   * @param rowNumber 1-based row number
+   */
+  public SpreadsheetLocation(String sheetName, int columnNumber, int rowNumber)
+  {
+    this.sheetName = sheetName;
+    this.columnNumber = columnNumber;
+    this.rowNumber = rowNumber;
+  }
 
-	public String getSheetName()
-	{
-		return this.sheetName;
-	}
+  public String getSheetName()
+  {
+    return this.sheetName;
+  }
 
-	public int getColumnNumber()
-	{
-		return columnNumber;
-	}
+  public int getColumnNumber()
+  {
+    return columnNumber;
+  }
 
-	public String getColumnName()
-	{
-		return SpreadSheetUtil.columnNumber2Name(columnNumber);
-	}
+  public String getColumnName()
+  {
+    return SpreadSheetUtil.columnNumber2Name(columnNumber);
+  }
 
-	public int getRowNumber()
-	{
-		return rowNumber;
-	}
+  public int getRowNumber()
+  {
+    return rowNumber;
+  }
 
-	public String getCellLocation()
-	{
-		return getColumnName() + rowNumber;
-	}
+  public String getCellLocation()
+  {
+    return getColumnName() + rowNumber;
+  }
 
-	public boolean equals(Object obj)
-	{
-		if (this == obj)
-			return true;
-		if ((obj == null) || (obj.getClass() != this.getClass()))
-			return false;
-		SpreadsheetLocation l = (SpreadsheetLocation)obj;
-		return (this.sheetName == l.sheetName && getColumnNumber() == l.getColumnNumber() && getRowNumber() == l.getRowNumber());
-	}
+  public boolean equals(Object obj)
+  {
+    if (this == obj)
+      return true;
+    if ((obj == null) || (obj.getClass() != this.getClass()))
+      return false;
+    SpreadsheetLocation l = (SpreadsheetLocation)obj;
+    return (this.sheetName == l.sheetName && getColumnNumber() == l.getColumnNumber() && getRowNumber() == l
+      .getRowNumber());
+  }
 
-	public int hashCode()
-	{
-		int hash = 12;
+  public int hashCode()
+  {
+    int hash = 12;
 
-		hash = hash + sheetName.hashCode();
-		hash = hash + columnNumber;
-		hash = hash + rowNumber;
-		return hash;
-	}
+    hash = hash + sheetName.hashCode();
+    hash = hash + columnNumber;
+    hash = hash + rowNumber;
+    return hash;
+  }
 
-	public String getFullyQualifiedLocation()
-	{
-		return "'" + getSheetName() + "'!" + getCellLocation();
-	}
+  public String getFullyQualifiedLocation()
+  {
+    return "'" + getSheetName() + "'!" + getCellLocation();
+  }
 
-	public String toString()
-	{
-		return getFullyQualifiedLocation();
-	}
+  public String toString()
+  {
+    return getFullyQualifiedLocation();
+  }
 }
---
+++ src/main/java/org/mm/test/IntegrationTestBase.java
@@ -0,0 +1,124 @@
+package org.mm.test;
+
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
+import org.mm.exceptions.MappingMasterException;
+import org.mm.parser.ASTExpression;
+import org.mm.parser.MappingMasterParser;
+import org.mm.parser.ParseException;
+import org.mm.parser.SimpleNode;
+import org.mm.parser.node.ExpressionNode;
+import org.mm.parser.node.MMExpressionNode;
+import org.mm.renderer.owlapi.OWLAPICoreRenderer;
+import org.mm.renderer.text.TextRenderer;
+import org.mm.rendering.owlapi.OWLAPIRendering;
+import org.mm.rendering.text.TextRendering;
+import org.mm.ss.SpreadSheetDataSource;
+import org.mm.ss.SpreadsheetLocation;
+import org.semanticweb.owlapi.model.OWLOntology;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+public class IntegrationTestBase
+{
+  /**
+   * Create a single sheet workbook. Not clear how to create an in-memory-only {@link Workbook} in JXL so
+   * we create a {@link WritableWorkbook} (which is not a subclass of {@link Workbook}) save it to a temporary file and
+   * then create a {@link Workbook} from it.
+   */
+  protected Workbook createWorkbook(String sheetName, Set<Label> cells)
+    throws IOException, WriteException, BiffException
+  {
+    File file = File.createTempFile("temp", "xlsx");
+    WritableWorkbook writableWorkbook = Workbook.createWorkbook(file);
+    WritableSheet sheet = writableWorkbook.createSheet(sheetName, 0);
+
+    for (Label cell : cells)
+      sheet.addCell(cell);
+
+    writableWorkbook.write();
+    writableWorkbook.close();
+
+    return Workbook.getWorkbook(file);
+  }
+
+  /**
+   * @param data Map of sheet name to cells
+   * @return A workbook
+   * @throws IOException
+   * @throws WriteException
+   * @throws BiffException
+   */
+  protected Workbook createWorkbook(Map<String, Set<Label>> data) throws IOException, WriteException, BiffException
+  {
+    File file = File.createTempFile("temp", "xlsx");
+    WritableWorkbook writableWorkbook = Workbook.createWorkbook(file);
+
+    int sheetIndex = 0;
+    for (String sheetName : data.keySet()) {
+      Set<Label> cells = data.get(sheetName);
+      WritableSheet sheet = writableWorkbook.createSheet(sheetName, sheetIndex++);
+
+      for (Label cell : cells)
+        sheet.addCell(cell);
+    }
+
+    writableWorkbook.write();
+    writableWorkbook.close();
+
+    return Workbook.getWorkbook(file);
+  }
+
+  protected SpreadSheetDataSource createSpreadsheetDataSource(String sheetName, Set<Label> cells)
+    throws BiffException, IOException, WriteException, MappingMasterException
+  {
+    Workbook workbook = createWorkbook(sheetName, cells);
+    return new SpreadSheetDataSource(workbook);
+  }
+
+  protected MMExpressionNode parseExpression(String expression) throws ParseException
+  {
+    MappingMasterParser parser = new MappingMasterParser(new ByteArrayInputStream(expression.getBytes()));
+    SimpleNode simpleNode = parser.expression();
+    ExpressionNode expressionNode = new ExpressionNode((ASTExpression)simpleNode);
+
+    return expressionNode.getMMExpressionNode();
+  }
+
+  protected Optional<? extends TextRendering> createTextRendering(String sheetName, Set<Label> cells,
+    SpreadsheetLocation currentLocation, String expression)
+    throws WriteException, BiffException, MappingMasterException, IOException, ParseException
+  {
+    SpreadSheetDataSource dataSource = createSpreadsheetDataSource(sheetName, cells);
+
+    dataSource.setCurrentLocation(currentLocation);
+
+    TextRenderer renderer = new TextRenderer(dataSource);
+    MMExpressionNode mmExpressionNode = parseExpression(expression);
+
+    return renderer.renderMMExpression(mmExpressionNode);
+  }
+
+  protected Optional<? extends OWLAPIRendering> createOWLAPIRendering(OWLOntology ontology, String sheetName,
+    Set<Label> cells, SpreadsheetLocation currentLocation, String expression)
+    throws WriteException, BiffException, MappingMasterException, IOException, ParseException
+  {
+    SpreadSheetDataSource dataSource = createSpreadsheetDataSource(sheetName, cells);
+
+    dataSource.setCurrentLocation(currentLocation);
+
+    OWLAPICoreRenderer renderer = new OWLAPICoreRenderer(ontology, dataSource);
+    MMExpressionNode mmExpressionNode = parseExpression(expression);
+
+    return renderer.renderMMExpression(mmExpressionNode);
+  }
+}
