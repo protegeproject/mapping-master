0f679c21fd6e41e78fb6f5a524dea91592eb7398
johardi <josef.hardi@stanford.edu>
2015-09-01 10:19:18 -0700

    Place the open file dialog to the top and change the button label to "Browse..."

--- src/main/java/org/mm/ui/view/DataSourceView.java
+++ src/main/java/org/mm/ui/view/DataSourceView.java
@@ -30,20 +30,18 @@ public class DataSourceView extends JPanel implements MMView
 
 		setLayout(new BorderLayout());
 
-		setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Workbook"));
-
 		tabSheetContainer = new JTabbedPane();
 		tabSheetContainer.addTab("NONE", new JPanel());
 		add(tabSheetContainer, BorderLayout.CENTER);
 
 		JPanel pnlWorkbookFile = new JPanel(new BorderLayout());
-		pnlWorkbookFile.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Workbook File"));
-		add(pnlWorkbookFile, BorderLayout.SOUTH);
+		pnlWorkbookFile.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Spreadsheet File"));
+		add(pnlWorkbookFile, BorderLayout.NORTH);
 
 		txtWorkbookPath = new JTextField("");
 		pnlWorkbookFile.add(txtWorkbookPath, BorderLayout.CENTER);
 
-		JButton cmdOpen = new JButton("Open");
+		JButton cmdOpen = new JButton("Browse...");
 		cmdOpen.addActionListener(new OpenWorkbookAction());
 		pnlWorkbookFile.add(cmdOpen, BorderLayout.EAST);
 
