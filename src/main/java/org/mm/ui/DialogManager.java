7482cb8ff258e5b6315e88a9706af56582ca8073
johardi <josef.hardi@stanford.edu>
2015-09-07 17:12:55 -0700

    Add a new showInputDialog option.

--- src/main/java/org/mm/ui/DialogManager.java
+++ src/main/java/org/mm/ui/DialogManager.java
@@ -11,6 +11,8 @@ public interface DialogManager
 {
 	int showConfirmDialog(Component parent, String title, String message);
 
+	String showInputDialog(Component parent, String message);
+
 	void showMessageDialog(Component parent, String message);
 
 	void showErrorMessageDialog(Component parent, String message);
