5b25e0b0d9b8eb37e223040908be0f6486036af3
johardi <josef.hardi@stanford.edu>
2015-09-14 12:13:33 -0700

    Add comment.

--- src/main/java/org/mm/ui/ModelView.java
+++ src/main/java/org/mm/ui/ModelView.java
@@ -1,5 +1,9 @@
 package org.mm.ui;
 
+/**
+ * To represent the models in the {@link org.mm.app} pacakge. Currently there are three models: ApplicationModel,
+ * DataSourceModel and MappingExpressionModel.
+ */
 public interface ModelView
 {
    void update();
