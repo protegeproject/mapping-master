bc37f5af656c2dfd010bb5767fa011d9f826152a
johardi <josef.hardi@stanford.edu>
2016-05-02 15:55:48 -0700

    Add Javadoc

--- src/main/java/org/mm/core/OWLEntityResolver.java
+++ src/main/java/org/mm/core/OWLEntityResolver.java
@@ -6,9 +6,38 @@ import org.semanticweb.owlapi.model.OWLEntity;
 
 public interface OWLEntityResolver
 {
-   <T extends OWLEntity> T resolve(String shortName, final Class<T> entityType)
+   /**
+    * Resolves the given entity name string and its type to an {@code OWLEntity} object.
+    * The method will check to the current active ontology and return the object
+    * found in the ontology.
+    *
+    * @param entityName
+    *          The entity name in short form or as a prefixed name string.
+    * @param entityType
+    *          The entity type following the OWLAPI class hierarchy. The types an be
+    *          one of these: {@link OWLClass}, {@linke OWLDataProperty},
+    *          {@link OWLObjectProperty}. {@link OWLNamedIndividual} or
+    *          {@link OWLDatatype}.
+    * @return The {@code OWLEntity} object with the specific type.
+    * @throws EntityNotFoundException If the entity name does not exist in the ontology.
+    */
+   <T extends OWLEntity> T resolve(String entityName, final Class<T> entityType)
          throws EntityNotFoundException;
 
-   <T extends OWLEntity> T create(String shortName, final Class<T> entityType)
+   /**
+    * Creates the {@code OWLEntity} object given the entity name string and its type.
+    * This method will reuse the entity object if the object can be found in the
+    * ontology.
+    *
+    * @param entityName
+    *        The entity name in short form or as a prefixed name string.
+    *          The entity type following the OWLAPI class hierarchy. The types an be
+    *          one of these: {@link OWLClass}, {@linke OWLDataProperty},
+    *          {@link OWLObjectProperty}. {@link OWLNamedIndividual} or
+    *          {@link OWLDatatype}.
+    * @return An {@code OWLEntity} object with the specific type.
+    * @throws EntityCreationException If the entity creation was failed
+    */
+   <T extends OWLEntity> T create(String entityName, final Class<T> entityType)
          throws EntityCreationException;
 }
