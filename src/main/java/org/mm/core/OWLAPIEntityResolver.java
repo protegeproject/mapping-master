7405721cb9a8f7be37a9cf690d14c089ee2d4247
johardi <josef.hardi@stanford.edu>
2016-10-03 11:49:19 -0700

    Create method interface for unchecked exception

--- src/main/java/org/mm/core/OWLAPIEntityResolver.java
+++ src/main/java/org/mm/core/OWLAPIEntityResolver.java
@@ -1,6 +1,10 @@
 package org.mm.core;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.util.Optional;
+
+import javax.annotation.Nonnull;
 
 import org.mm.exceptions.EntityCreationException;
 import org.mm.exceptions.EntityNotFoundException;
@@ -16,61 +20,95 @@ import org.semanticweb.owlapi.model.OWLObjectProperty;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.PrefixManager;
 
-public class OWLAPIEntityResolver implements OWLEntityResolver
-{
-   private OWLOntology ontology;
-   private OWLDataFactory owlDataFactory;
-   private PrefixManager prefixManager;
+public class OWLAPIEntityResolver implements OWLEntityResolver {
 
-   public OWLAPIEntityResolver(OWLOntology ontology, PrefixManager prefixManager)
-   {
-      this.ontology = ontology;
-      this.prefixManager = prefixManager;
-      owlDataFactory = ontology.getOWLOntologyManager().getOWLDataFactory();
+   private final OWLOntology ontology;
+   private final PrefixManager prefixManager;
+   private final OWLDataFactory dataFactory;
+
+   public OWLAPIEntityResolver(@Nonnull OWLOntology ontology, @Nonnull PrefixManager prefixManager) {
+      this.ontology = checkNotNull(ontology);
+      this.prefixManager = checkNotNull(prefixManager);
+      dataFactory = ontology.getOWLOntologyManager().getOWLDataFactory();
    }
 
    @Override
    public <T extends OWLEntity> T resolve(String entityName, final Class<T> entityType)
-         throws EntityNotFoundException
-   {
+         throws EntityNotFoundException {
       IRI entityIRI = prefixManager.getIRI(entityName);
       Optional<OWLEntity> foundEntity = ontology.getEntitiesInSignature(entityIRI).stream().findFirst();
       if (foundEntity.isPresent()) {
          try {
             return entityType.cast(foundEntity.get());
-         }
-         catch (ClassCastException e) {
-            String template = "The expected entity '%s' does not have type: %s";
-            throw new EntityNotFoundException(String.format(template, entityName, entityType.getSimpleName()));
+         } catch (ClassCastException e) {
+            throw new EntityNotFoundException(
+                  String.format("The expected entity name '%s' does not have type %s",
+                  entityName, entityType.getSimpleName()));
          }
       }
-      String template = "The expected entity '%s' does not exist in the ontology";
-      throw new EntityNotFoundException(String.format(template, entityName));
+      throw new EntityNotFoundException(
+            String.format("The expected entity name '%s' does not exist in the ontology",
+                  entityName));
    }
 
    @Override
+   public <T extends OWLEntity> T resolveUnchecked(String entityName, Class<T> entityType) {
+      try {
+         return resolve(entityName, entityType);
+      } catch (EntityNotFoundException e) {
+         throw new RuntimeException(e.getMessage());
+      }
+   }
+
+   /**
+    * Creates an OWL entity following its given name and type. The method will first look
+    * the entity name in the loaded ontology and reuse the same object. If no entity was
+    * found, this method will create a new object
+    *
+    * @param entityName
+    *          The entity name in short form or as a prefixed name string.
+    * @param entityType
+    *          The entity type following the OWLAPI class hierarchy. The types an be
+    *          one of these: {@link OWLClass}, {@link OWLDataProperty},
+    *          {@link OWLObjectProperty}. {@link OWLNamedIndividual} or
+    *          {@link OWLDatatype}.
+    * @return Returns an OWL entity object following its given name and type.
+    * @throws EntityCreationException If the entity creation was failed
+    */
+   @Override
    public <T extends OWLEntity> T create(String entityName, final Class<T> entityType)
-         throws EntityCreationException
-   {
+         throws EntityCreationException {
       IRI entityIRI = prefixManager.getIRI(entityName);
       Optional<OWLEntity> foundEntity = ontology.getEntitiesInSignature(entityIRI).stream().findFirst();
-      if (foundEntity.isPresent()) {
-         return entityType.cast(foundEntity.get());
-      } else {
-         if (OWLClass.class.isAssignableFrom(entityType)) {
-            return entityType.cast(owlDataFactory.getOWLClass(entityName, prefixManager));
-         } else if (OWLObjectProperty.class.isAssignableFrom(entityType)) {
-            return entityType.cast(owlDataFactory.getOWLObjectProperty(entityName, prefixManager));
-         } else if (OWLDataProperty.class.isAssignableFrom(entityType)) {
-            return entityType.cast(owlDataFactory.getOWLDataProperty(entityName, prefixManager));
-         } else if (OWLNamedIndividual.class.isAssignableFrom(entityType)) {
-            return entityType.cast(owlDataFactory.getOWLNamedIndividual(entityName, prefixManager));
-         } else if (OWLAnnotationProperty.class.isAssignableFrom(entityType)) {
-            return entityType.cast(owlDataFactory.getOWLAnnotationProperty(entityName, prefixManager));
-         } else if (OWLDatatype.class.isAssignableFrom(entityType)) {
-            return entityType.cast(owlDataFactory.getOWLDatatype(entityName, prefixManager));
-         }
+      if (!foundEntity.isPresent()) {
+         return createNew(entityName, entityType);
       }
-      throw new IllegalStateException("Programmer error - report this (with stack trace) to the Protege mailing list");
+      return entityType.cast(foundEntity.get());
+   }
+
+   @Override
+   public <T extends OWLEntity> T createUnchecked(String entityName, Class<T> entityType) {
+      try {
+         return create(entityName, entityType);
+      } catch (EntityCreationException e) {
+         throw new RuntimeException(e.getMessage());
+      }
+   }
+
+   private <T extends OWLEntity> T createNew(String entityName, final Class<T> entityType) {
+      if (OWLClass.class.isAssignableFrom(entityType)) {
+         return entityType.cast(dataFactory.getOWLClass(entityName, prefixManager));
+      } else if (OWLObjectProperty.class.isAssignableFrom(entityType)) {
+         return entityType.cast(dataFactory.getOWLObjectProperty(entityName, prefixManager));
+      } else if (OWLDataProperty.class.isAssignableFrom(entityType)) {
+         return entityType.cast(dataFactory.getOWLDataProperty(entityName, prefixManager));
+      } else if (OWLNamedIndividual.class.isAssignableFrom(entityType)) {
+         return entityType.cast(dataFactory.getOWLNamedIndividual(entityName, prefixManager));
+      } else if (OWLAnnotationProperty.class.isAssignableFrom(entityType)) {
+         return entityType.cast(dataFactory.getOWLAnnotationProperty(entityName, prefixManager));
+      } else if (OWLDatatype.class.isAssignableFrom(entityType)) {
+         return entityType.cast(dataFactory.getOWLDatatype(entityName, prefixManager));
+      }
+      throw new IllegalStateException(String.format("Unknown entity type %s", entityType));
    }
 }
--- src/main/java/org/mm/core/OWLEntityResolver.java
+++ src/main/java/org/mm/core/OWLEntityResolver.java
@@ -2,43 +2,75 @@ package org.mm.core;
 
 import org.mm.exceptions.EntityCreationException;
 import org.mm.exceptions.EntityNotFoundException;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLDatatype;
 import org.semanticweb.owlapi.model.OWLEntity;
+import org.semanticweb.owlapi.model.OWLNamedIndividual;
+import org.semanticweb.owlapi.model.OWLObjectProperty;
 
 public interface OWLEntityResolver
 {
    /**
-    * Resolves the given entity name string and its type to an {@code OWLEntity} object.
-    * The method will check to the current active ontology and return the object
-    * found in the ontology.
+    * Resolves the given entity name and returns the OWL entity object with the specified type.
+    * The method will look the entity name in the active ontology and return the found object.
     *
     * @param entityName
     *          The entity name in short form or as a prefixed name string.
     * @param entityType
     *          The entity type following the OWLAPI class hierarchy. The types an be
-    *          one of these: {@link OWLClass}, {@linke OWLDataProperty},
+    *          one of these: {@link OWLClass}, {@link OWLDataProperty},
     *          {@link OWLObjectProperty}. {@link OWLNamedIndividual} or
     *          {@link OWLDatatype}.
-    * @return The {@code OWLEntity} object with the specific type.
+    * @return Returns an OWL entity object according to its type.
     * @throws EntityNotFoundException If the entity name does not exist in the ontology.
     */
    <T extends OWLEntity> T resolve(String entityName, final Class<T> entityType)
          throws EntityNotFoundException;
 
    /**
-    * Creates the {@code OWLEntity} object given the entity name string and its type.
-    * This method will reuse the entity object if the object can be found in the
-    * ontology.
+    * Resolves the given entity name and returns the OWL entity object with the specified type.
+    * The method will look the entity name in the active ontology and return the found object.
+    * Unlike {@code resolve(String, Class<T>)}, this method does not throw a checked exception
     *
     * @param entityName
-    *        The entity name in short form or as a prefixed name string.
+    *          The entity name in short form or as a prefixed name string.
     * @param entityType
     *          The entity type following the OWLAPI class hierarchy. The types an be
-    *          one of these: {@link OWLClass}, {@linke OWLDataProperty},
+    *          one of these: {@link OWLClass}, {@link OWLDataProperty},
     *          {@link OWLObjectProperty}. {@link OWLNamedIndividual} or
     *          {@link OWLDatatype}.
-    * @return An {@code OWLEntity} object with the specific type.
+    * @return Returns an OWL entity object according to its type.
+    */
+   <T extends OWLEntity> T resolveUnchecked(String entityName, final Class<T> entityType);
+
+   /**
+    * Creates an OWL entity following its given name and type.
+    *
+    * @param entityName
+    *          The entity name in short form or as a prefixed name string.
+    * @param entityType
+    *          The entity type following the OWLAPI class hierarchy. The types an be
+    *          one of these: {@link OWLClass}, {@link OWLDataProperty},
+    *          {@link OWLObjectProperty}. {@link OWLNamedIndividual} or
+    *          {@link OWLDatatype}.
+    * @return Returns an OWL entity object following its given name and type
     * @throws EntityCreationException If the entity creation was failed
     */
    <T extends OWLEntity> T create(String entityName, final Class<T> entityType)
          throws EntityCreationException;
+
+   /**
+    * Creates an OWL entity following its given name and type. Unlike {@code create(String, Class<T>)},
+    * this method does not throw a checked exception.
+    *
+    * @param entityName
+    *          The entity name in short form or as a prefixed name string.
+    * @param entityType
+    *          The entity type following the OWLAPI class hierarchy. The types an be
+    *          one of these: {@link OWLClass}, {@link OWLDataProperty},
+    *          {@link OWLObjectProperty}. {@link OWLNamedIndividual} or
+    *          {@link OWLDatatype}.
+    * @return Returns an OWL entity object following its given name and type
+    */
+   <T extends OWLEntity> T createUnchecked(String entityName, final Class<T> entityType);
 }
