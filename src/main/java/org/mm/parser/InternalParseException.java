c6d94ff914a156b1c842b64e9b56cd55a8c2677b
johardi <josef.hardi@stanford.edu>
2016-11-29 12:50:28 -0800

    Clean up old parser files.

--- src/main/java/org/mm/parser/InternalParseException.java
+++
@@ -1,8 +0,0 @@
-package org.mm.parser;
-
-public class InternalParseException extends ParseException
-{
-  public InternalParseException() { super(); }
-
-  public InternalParseException(String s) { super(s); }
-}
