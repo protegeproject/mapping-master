760a24c8bd8934671db16a5f3213f5428b002c7b
johardi <josef.hardi@stanford.edu>
2017-02-20 14:19:17 -0800

    Add test cases for the if-entity-absent directives.
    Note that there are 3 tests that are ignored currently, i.e.,
    shouldIgnoreIfEntityIsAbsent(), shouldWarnIfEntityIsAbsent() and
    shouldFailIfEntityIsAbsent(). The reason is because the API is no longer
    interact with the reference directives once it reaches the OWL object
    creation code.
    
    The fix will require a small refactor to allow the OwlFactory to access
    the reference directives. However, since the usage of these directives
    is an edge case, we are going to put this issue as a pending case.

---
+++ src/test/java/org/mm/renderer/owl/OrderIfEntityAbsentDirectiveTest.java
@@ -0,0 +1,87 @@
+package org.mm.renderer.owl;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasSize;
+import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.Declaration;
+
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.semanticweb.owlapi.model.OWLAxiom;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+
+/**
+ * @author Josef Hardi <josef.hardi@stanford.edu> <br>
+ *         Stanford Center for Biomedical Informatics Research
+ */
+public class OrderIfEntityAbsentDirectiveTest extends OwlRendererTest {
+
+   @Rule
+   public final ExpectedException thrown = ExpectedException.none();
+
+   @Before
+   public void setUp() throws OWLOntologyCreationException {
+      createEmptyExcelWorkbook();
+      createEmptyOWLOntology();
+   }
+
+   @Test
+   public void shouldUseDefaultResponse() {
+      // Arrange
+      addCell("Sheet1", 1, 1, "Car");
+      // Act
+      Set<OWLAxiom> result = evaluate("Class: @A1");
+      // Assert
+      assertThat(result, hasSize(1));
+      assertThat(result, containsInAnyOrder(Declaration(Vocabulary.CAR)));
+   }
+
+   @Test
+   @Ignore("Unsupported by the API")
+   public void shouldIgnoreIfEntityIsAbsent() {
+      // Arrange
+      addCell("Sheet1", 1, 1, "Car");
+      // Act
+      Set<OWLAxiom> result = evaluate("Class: @A1(mm:ignoreIfEntityAbsent)");
+      // Assert
+      assertThat(result, hasSize(0));
+   }
+
+   @Test
+   public void shouldCreateIfEntityIsAbsent() {
+      // Arrange
+      addCell("Sheet1", 1, 1, "Car");
+      // Act
+      Set<OWLAxiom> result = evaluate("Class: @A1(mm:createIfEntityAbsent)");
+      // Assert
+      assertThat(result, hasSize(1));
+      assertThat(result, containsInAnyOrder(Declaration(Vocabulary.CAR)));
+   }
+
+   @Test
+   @Ignore("Unsupported by the API")
+   public void shouldWarnIfEntityIsAbsent() {
+      // Arrange
+      addCell("Sheet1", 1, 1, "Car");
+      // Act
+      Set<OWLAxiom> result = evaluate("Class: @A1(mm:warningIfEntityAbsent)");
+      // Assert
+      assertThat(result, hasSize(0));
+   }
+
+   @Test
+   @Ignore("Unsupported by the API")
+   public void shouldFailIfEntityIsAbsent() {
+      // Arrange
+      addCell("Sheet1", 1, 1, "Car");
+      // Act
+      Set<OWLAxiom> result = evaluate("Class: @A1(mm:errorIfEntityAbsent)");
+      // Assert
+      assertThat(result, hasSize(0));
+   }
+}
