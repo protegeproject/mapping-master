0c00db2b13771afe0254c4151dca84e7bd6affd0
johardi <josef.hardi@stanford.edu>
2019-07-01 09:54:49 -0700

    fix(renderer): returns null when the the sheet doesn't exist in the workbook

--- src/main/java/org/mm/renderer/Workbook.java
+++ src/main/java/org/mm/renderer/Workbook.java
@@ -2,6 +2,7 @@ package org.mm.renderer;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.mm.renderer.internal.CellAddress;
 
@@ -25,8 +26,10 @@ public class Workbook {
       return new Sheet(workbook.createSheet(sheetName));
    }
 
+   @Nullable
    public Sheet getSheet(String sheetName) {
-      return new Sheet(workbook.getSheet(sheetName));
+      org.apache.poi.ss.usermodel.Sheet sheet = workbook.getSheet(sheetName);
+      return (sheet != null) ? new Sheet(sheet) : null;
    }
 
    public Sheet getSheet(int sheetIndex) {
