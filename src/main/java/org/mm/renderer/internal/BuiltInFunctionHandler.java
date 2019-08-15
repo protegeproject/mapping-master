1a791fcecdef1fb9ecf077dac48b746a92ca94c1
johardi <josef.hardi@stanford.edu>
2019-08-15 12:43:04 -0700

    feature(builtin-function): implement TO_LOWER_CASE function

--- src/main/java/org/mm/renderer/internal/BuiltInFunctionHandler.java
+++ src/main/java/org/mm/renderer/internal/BuiltInFunctionHandler.java
@@ -12,12 +12,19 @@ public class BuiltInFunctionHandler implements MappingMasterParserConstants {
       int functionType = function.getFunctionType();
       switch (functionType) {
          case MM_TO_UPPER_CASE: return handleToUpperCase(inputValue);
+         case MM_TO_LOWER_CASE: return handleToLowerCase(inputValue);
          default: return inputValue;
       }
    }
 
    private Value handleToUpperCase(Value inputValue) {
       String newString = inputValue.getString().toUpperCase();
+      Value outputValue = inputValue.update(newString);
+      return outputValue;
+   }
+
+   private Value handleToLowerCase(Value inputValue) {
+      String newString = inputValue.getString().toLowerCase();
       Value outputValue = inputValue.update(newString);
       return outputValue;
    }
