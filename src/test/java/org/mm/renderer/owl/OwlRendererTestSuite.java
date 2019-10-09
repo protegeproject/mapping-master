93e76264ad2b9473c0f45428dc7705d9168f12b4
johardi <josef.hardi@stanford.edu>
2019-10-09 10:13:15 -0700

    test: add class expression test in the test suite

--- src/test/java/org/mm/renderer/owl/OwlRendererTestSuite.java
+++ src/test/java/org/mm/renderer/owl/OwlRendererTestSuite.java
@@ -6,6 +6,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
    EntityDeclarationTest.class,
+   ClassExpressionTest.class,
    AssertionAxiomTest.class,
    IriEncodingDirectiveTest.class,
    EmptyCellDirectiveTest.class,
